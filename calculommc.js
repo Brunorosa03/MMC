740f0472c43af1855f6d70cb1617fd4346fe3650
Bruno Rosa <bruno.rosa2812@gmail.com>
2024-06-25 19:01:35 -0300

    Cálculo de MMC

---
+++ calculommc.js
@@ -0,0 +1,36 @@
+let numero1
+let numero2
+
+process.stdin.on("data", function(data) {
+    let input = +data.toString().trim()
+
+    if(!numero1) {
+
+        numero1 = input
+    } else {
+        numero2 = input
+
+        let i
+        let maior
+        let menor
+
+        if(numero1 > numero2) {
+            maior = numero1
+            menor = numero2
+        } else {
+            maior = numero2
+            menor = numero1
+        }
+
+        i = maior
+        for (i; i <= numero1 * numero2; i += maior) {
+            if(i % numero1 == 0 && i % numero2 == 0) {
+                console.log("MMC: " + i)
+                break
+            }
+        }
+
+        
+        process.exit()
+    }
+})
